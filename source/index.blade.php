4b2c7e62854e6e947c33a283483587c153358fb2
Ranie Santos <raniesantos@users.noreply.github.com>
2020-05-08 11:12:33 +0800

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,12 +3,9 @@
 @section('content')
     <h1>Welcome to Artisan Static for Jigsaw</h1>
 
-    <h2>Currently being updated, need suggestions</h2>
+    <h2>Currently being updated</h2>
 
-    <ul>
-        <li>See temporary repo <a href="https://github.com/raniesantos/artisan-static-new">here</a></li>
-        <li>I'm trying to decide on a new contact form endpoint since Jumprock is dead, leave suggestions <a href="https://github.com/raniesantos/artisan-static-new/issues/1">here</a></li>
-    </ul>
+    <p>See temporary repo <a href="https://github.com/raniesantos/artisan-static-new">here</a></p>
 
     <h2>Asset Compilation Test</h2>
 
